af1a10f9250f427ac13e2b777c0c367838cb78a5
harshavinnu <119720338+harshavinnu@users.noreply.github.com>
2023-08-18 15:12:38 +0530

    x=input() if x=='V':     print("Violet") elif x=='I':     print("Indigo") elif x=='B':     print("Blue") elif x=='G':     print("Green") elif x=='Y':     print("Yellow") elif x=='O':     print("Orange") elif x=='R':     print("Red") else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+x=input()
+if x=='V':
+    print("Violet")
+elif x=='I':
+    print("Indigo")
+elif x=='B':
+    print("Blue")
+elif x=='G':
+    print("Green")
+elif x=='Y':
+    print("Yellow")
+elif x=='O':
+    print("Orange")
+elif x=='R':
+    print("Red")
+else:
+    print(-1)
